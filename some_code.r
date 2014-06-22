f2a8ba51d3a0ad8041dd8225cf419476633a7665
velavar <neehaarika90@gmail.com>
2014-06-23 01:42:01 +0530

    Changed r file

--- some_code.r
+++ some_code.r
@@ -2,7 +2,7 @@ library(caret);
 library(kernlab);
 library(scatterplot3d);
 
-dat=read.csv("C:\\Users\\velavarthy\\Desktop\\study7\\pml-training.csv",header=TRUE)
+dat=read.csv("pml-training.csv",header=TRUE)
 dattemp<-dat[,!sapply(dat,function(x) any(is.na(x)))]
 dat1<-dattemp[,!sapply(dattemp, function(x) any(x == ""))]
 inTrain=createDataPartition(y=dat1$classe,p=0.70,list=FALSE)
@@ -15,11 +15,9 @@ modFit
 newclass<-predict(modFit,newdata=testing)
 confusionMatrix(newclass,testing$classe)
 
-testdat=read.csv("C:\\Users\\nvelavar\\Desktop\\other\\pml-testing.csv",header=TRUE)
+testdat=read.csv("pml-testing.csv",header=TRUE)
 newclass<-predict(modFit,newdata=testdat)
 newclass
-
-
 
 modFit<-train(classe~.,method="rf",data=training,trControl=Control,importance=TRUE)
 varImpPlot(modFit$finalModel)
